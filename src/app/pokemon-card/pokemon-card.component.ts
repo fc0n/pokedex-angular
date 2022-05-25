a94832745fcf9da9b0908f274fadaac4164b1af1
fc0n <jeangabrieljb9@gmail.com>
2022-05-25 15:07:39 -0300

    pokemon service data from api

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Pokemon } from 'src/_model/pokemon';
+import { getPokemonImage, getPokemonNumber, Pokemon } from '../../_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -7,16 +7,11 @@ import { Pokemon } from 'src/_model/pokemon';
   styleUrls: ['./pokemon-card.component.sass']
 })
 
-export class PokemonCardComponent{
+export class PokemonCardComponent {
   @Input()
   public pokemon!: Pokemon;
+  
+  public getPokemonImage = getPokemonImage;
 
-  public leadingZero(str: string | number, size: number = 3): string {
-    let s = String(str);
-
-    while (s.length < (size || 2)){
-      s = '0' + s;
-  }
-    return s;
-  }
+  public getPokemonNumber = getPokemonNumber;
 }
